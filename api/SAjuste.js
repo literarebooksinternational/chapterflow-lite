dad2c100330d38a9a90a320ff72c48e79f064e9e
Otavio <otaviodias.cco@gmail.com>
2025-08-13 15:20:35 -0300

    ajustado ANON Key

--- api/SAjuste.js
+++ api/SAjuste.js
@@ -7,7 +7,7 @@ dotenv.config();
 // Inicializa Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 // Configuração do nodemailer
