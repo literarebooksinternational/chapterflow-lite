55b9d1dc12f3ca93006093ee91593c42744b617d
Otavio <otaviodias.cco@gmail.com>
2025-08-13 16:12:22 -0300

    Retirado teste de e-mail

--- api/SAjuste.js
+++ api/SAjuste.js
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 dotenv.config();
 
 // Usando a chave de serviço para operações que requerem permissões elevadas
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
 const transporter = nodemailer.createTransport({
   host: "smtp.literarebooks.com.br",
